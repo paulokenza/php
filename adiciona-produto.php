caa440e0ab8109f5cb5dfc92a1741371836749aa
Paulo Henrique de Oliveira Silva <paulokenza@gmail.com>
2019-02-03 23:50:06 -0200

    Adicionado preço do produto

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -2,8 +2,14 @@
 
 <?php
  $produto=$_GET["produto"];
+
+ $preco=$_GET["preco"];
 ?>
 
 Produto <?php echo $produto; ?> adicionado com sucesso!
+
+<br/>
+
+Preço <?php echo $preco; ?> Reais.
 
 </html>
