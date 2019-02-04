85cbb74a102f288e6f6eb43bc6cc6a998b114c94
Paulo Henrique de Oliveira Silva <paulokenza@gmail.com>
2019-02-03 23:45:02 -0200

    Adicionando os primeiros arquivos

---
+++ adiciona-produto.php
@@ -0,0 +1,9 @@
+<html>
+
+<?php
+ $produto=$_GET["produto"];
+?>
+
+Produto <?php echo $produto; ?> adicionado com sucesso!
+
+</html>
---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<?php $nome = "PH, agora vai"; ?>
+Loja do <?php echo $nome; ?>
+</html>
+
+/**
+ * Created by PhpStorm.
+ * User: henrique
+ * Date: 2/3/19
+ * Time: 11:01 PM
+ */
